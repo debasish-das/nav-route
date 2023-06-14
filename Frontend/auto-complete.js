f18a0ecd4e07add1f80f2aed0a95b958da59ea0b
Debasish Das <debasish.das64@yahoo.com>
2023-06-13 23:11:30 -0700

    google map distance matrix test, .net web api test

--- Frontend/auto-complete.js
+++ Frontend/auto-complete.js
@@ -13,7 +13,7 @@ function initAutocomplete() {
   // Create the autocomplete object, restricting the search to geographical
   // location types.
   autocomplete = new google.maps.places.Autocomplete(
-    /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+    (document.getElementById('autocomplete')),
     {types: ['geocode']});
 
   // When the user selects an address from the dropdown, populate the address
