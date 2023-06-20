8428f2c173437ebf6acd05af3dcc4231680b97f9
Debasish Das <debasish.das64@yahoo.com>
2023-06-20 14:49:36 -0700

    Calling google distance matrix, and generating permutation

--- Frontend/scripts/main.js
+++ Frontend/scripts/main.js
@@ -3,14 +3,15 @@
 * Author: Group#5 (Das, Nithish, Sanad)
 */
 
-let appVars = {
-    startingPoints: [],
-    destinations: []
-}
+import { addInputFieldForPlaces } from './PlaceInput.js';
+import { appVars } from './appVars.js';
 
 function initApp() {
     addInputFieldForPlaces();
     addEventListeners();
+    
+    let arr = [1,2,3,4];
+    getPermutation(arr.length, arr);
 }
 
 window.initApp = initApp;
@@ -18,58 +19,205 @@ window.initApp = initApp;
 function addEventListeners() {
     [
         { selector: "#add-place-btn", eventFunction: addInputFieldForPlaces },
+        { selector: "#generate-route-btn", eventFunction: generateRoutes },
     ].forEach(item => {
         document.querySelector(item.selector).addEventListener("click", item.eventFunction, false)
     })
 }
 
-function addInputFieldForPlaces(event) {
-    let inputDiv = document.createElement("div");
-    inputDiv.className = "input-group mb-3";
-    
-    let input = document.createElement("input");
-    input.setAttribute("class", "form-control");
-    inputDiv.appendChild(input);
-
-    let timeId = new Date().valueOf();
-    inputDiv.setAttribute("time-id", timeId);
-
-    let isStartPoint = event && event.target.id === "add-place-btn" ? false : true;
-    if (isStartPoint) {
-        input.setAttribute("placeholder", "Enter starting point");
-        document.getElementById("places").prepend(inputDiv);
+function generateRoutes() {
+    if (appVars.startingPoints.length && appVars.startingPoints[0].mapInfo) {
+        let startingPoint = appVars.startingPoints[0];
+        let destinations = appVars.destinations.filter(x => x.mapInfo);
+        if (destinations.length) {
+            let places = [];
+            places.push(startingPoint.mapInfo.geometry.location)
+            destinations.forEach(d => {
+                places.push(d.mapInfo.geometry.location)
+            })
+            getDistanceMatrix(places)
+        }
+        else {
+            alert("Please enter valid destination");
+        }
     }
     else {
-        let crossBtn = document.createElement("span");
-        crossBtn.innerHTML = `❌`
-        crossBtn.className = "input-group-text";
-        crossBtn.style = "cursor:pointer";
-        crossBtn.timeId = timeId
-        crossBtn.addEventListener("click", romovePlaceInputField);
-
-        input.setAttribute("placeholder", "Enter destination");
-        inputDiv.appendChild(crossBtn);
-        document.getElementById("destinations").prepend(inputDiv);
+        alert("Please enter starting point");
     }
-
-    let placeType = isStartPoint ? "startingPoints" : "destinations";
-    let place = {
-        id: timeId,
-        mapInfo: null
-    }
-    appVars[placeType].push(place);
-
-    let autocomplete = new google.maps.places.Autocomplete(
-        input,
-        { types: ['geocode'] });
-    autocomplete.addListener('place_changed', () => {
-        place.mapInfo = autocomplete.getPlace();
-    });
 }
 
-function romovePlaceInputField(event) {
-    let parentNode = event.target.closest("[time-id]");
-    let timeId = parentNode.getAttribute("time-id");
-    appVars.destinations = appVars.destinations.filter(x => x.id != timeId);
-    parentNode.remove();
+function getDistanceMatrix(places) {
+    const request = {
+        origins: places,
+        destinations: places,
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC
+    };
+    const service = new google.maps.DistanceMatrixService();
+    // service.getDistanceMatrix(request).then((response) => {
+    //     if(response) {
+    //         getRoutes(response)
+    //     }
+    //     else {
+    //         alert("Could not found any route");
+    //     }
+    // })
+
+    getRoutes({
+        "rows": [
+            {
+                "elements": [
+                    {
+                        "distance": {
+                            "text": "1 m",
+                            "value": 0
+                        },
+                        "duration": {
+                            "text": "1 min",
+                            "value": 0
+                        },
+                        "status": "OK"
+                    },
+                    {
+                        "distance": {
+                            "text": "3.5 km",
+                            "value": 3503
+                        },
+                        "duration": {
+                            "text": "8 mins",
+                            "value": 484
+                        },
+                        "status": "OK"
+                    },
+                    {
+                        "distance": {
+                            "text": "5.7 km",
+                            "value": 5723
+                        },
+                        "duration": {
+                            "text": "14 mins",
+                            "value": 820
+                        },
+                        "status": "OK"
+                    }
+                ]
+            },
+            {
+                "elements": [
+                    {
+                        "distance": {
+                            "text": "3.4 km",
+                            "value": 3410
+                        },
+                        "duration": {
+                            "text": "8 mins",
+                            "value": 478
+                        },
+                        "status": "OK"
+                    },
+                    {
+                        "distance": {
+                            "text": "1 m",
+                            "value": 0
+                        },
+                        "duration": {
+                            "text": "1 min",
+                            "value": 0
+                        },
+                        "status": "OK"
+                    },
+                    {
+                        "distance": {
+                            "text": "6.9 km",
+                            "value": 6908
+                        },
+                        "duration": {
+                            "text": "15 mins",
+                            "value": 913
+                        },
+                        "status": "OK"
+                    }
+                ]
+            },
+            {
+                "elements": [
+                    {
+                        "distance": {
+                            "text": "5.7 km",
+                            "value": 5725
+                        },
+                        "duration": {
+                            "text": "14 mins",
+                            "value": 813
+                        },
+                        "status": "OK"
+                    },
+                    {
+                        "distance": {
+                            "text": "7.4 km",
+                            "value": 7401
+                        },
+                        "duration": {
+                            "text": "14 mins",
+                            "value": 868
+                        },
+                        "status": "OK"
+                    },
+                    {
+                        "distance": {
+                            "text": "1 m",
+                            "value": 0
+                        },
+                        "duration": {
+                            "text": "1 min",
+                            "value": 0
+                        },
+                        "status": "OK"
+                    }
+                ]
+            }
+        ],
+        "originAddresses": [
+            "4293 Perry St, Vancouver, BC V5N 3X6, Canada",
+            "2316 Nanaimo St, Vancouver, BC V5N 5E2, Canada",
+            "6154 Willingdon Ave, Burnaby, BC V5H, Canada"
+        ],
+        "destinationAddresses": [
+            "4293 Perry St, Vancouver, BC V5N 3X6, Canada",
+            "2316 Nanaimo St, Vancouver, BC V5N 5E2, Canada",
+            "6154 Willingdon Ave, Burnaby, BC V5H, Canada"
+        ]
+    })
+}
+
+// function getRoutes(distanceMatrix) {
+//     let routes = [];
+//     for (let i = 0; index < distanceMatrix.rows.length; i++) {
+//         const from = distanceMatrix.originAddresses[i];
+//         for (let j = 0; j < array.length; j++) {
+//             const element = array[j];
+
+//         }
+//         const from = distanceMatrix.rows[i];
+
+//     }
+// }
+
+
+function getPermutation(k, arr) {
+    if (k === 1) {
+        console.log(arr)
+    }
+    else {
+        for (let i = 0; i < k-1; i++) {
+            getPermutation(k - 1, arr)
+            if (k % 2 == 0) {
+                [arr[k - 1], arr[i]] = [arr[i], arr[k - 1]]
+            }
+            else {
+                [arr[k - 1], arr[0]] = [arr[0], arr[k - 1]]
+            }
+            getPermutation(k - 1, arr)
+        }
+    }
 }
