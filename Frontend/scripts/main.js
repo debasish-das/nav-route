ea8c858432cab694f8373b822e0bb75e3b275cb8
Debasish Das <debasish.das64@yahoo.com>
2023-06-20 21:18:34 -0700

    generating all the routes and fixing permute issue

--- Frontend/scripts/main.js
+++ Frontend/scripts/main.js
@@ -9,9 +9,6 @@ import { appVars } from './appVars.js';
 function initApp() {
     addInputFieldForPlaces();
     addEventListeners();
-    
-    let arr = [1,2,3,4];
-    getPermutation(arr.length, arr);
 }
 
 window.initApp = initApp;
@@ -35,7 +32,22 @@ function generateRoutes() {
             destinations.forEach(d => {
                 places.push(d.mapInfo.geometry.location)
             })
-            getDistanceMatrix(places)
+
+            const request = {
+                origins: places,
+                destinations: places,
+                travelMode: google.maps.TravelMode.DRIVING,
+                unitSystem: google.maps.UnitSystem.METRIC
+            };
+            const service = new google.maps.DistanceMatrixService();
+            service.getDistanceMatrix(request).then((res) => {
+                if (res) {
+                    getRoutesFromDistancMatrix(res);
+                }
+                else {
+                    alert("Could not found any route");
+                }
+            })
         }
         else {
             alert("Please enter valid destination");
@@ -46,179 +58,56 @@ function generateRoutes() {
     }
 }
 
-function getDistanceMatrix(places) {
-    const request = {
-        origins: places,
-        destinations: places,
-        travelMode: google.maps.TravelMode.DRIVING,
-        unitSystem: google.maps.UnitSystem.METRIC
-    };
-    const service = new google.maps.DistanceMatrixService();
-    // service.getDistanceMatrix(request).then((response) => {
-    //     if(response) {
-    //         getRoutes(response)
-    //     }
-    //     else {
-    //         alert("Could not found any route");
-    //     }
-    // })
+function getRoutesFromDistancMatrix(res) {
+    let dstIndexes = [];
+    for (let i = 1; i < res.destinationAddresses.length; i++) {
+        dstIndexes.push(i);
+    }
 
-    getRoutes({
-        "rows": [
-            {
-                "elements": [
-                    {
-                        "distance": {
-                            "text": "1 m",
-                            "value": 0
-                        },
-                        "duration": {
-                            "text": "1 min",
-                            "value": 0
-                        },
-                        "status": "OK"
-                    },
-                    {
-                        "distance": {
-                            "text": "3.5 km",
-                            "value": 3503
-                        },
-                        "duration": {
-                            "text": "8 mins",
-                            "value": 484
-                        },
-                        "status": "OK"
-                    },
-                    {
-                        "distance": {
-                            "text": "5.7 km",
-                            "value": 5723
-                        },
-                        "duration": {
-                            "text": "14 mins",
-                            "value": 820
-                        },
-                        "status": "OK"
-                    }
-                ]
-            },
-            {
-                "elements": [
-                    {
-                        "distance": {
-                            "text": "3.4 km",
-                            "value": 3410
-                        },
-                        "duration": {
-                            "text": "8 mins",
-                            "value": 478
-                        },
-                        "status": "OK"
-                    },
-                    {
-                        "distance": {
-                            "text": "1 m",
-                            "value": 0
-                        },
-                        "duration": {
-                            "text": "1 min",
-                            "value": 0
-                        },
-                        "status": "OK"
-                    },
-                    {
-                        "distance": {
-                            "text": "6.9 km",
-                            "value": 6908
-                        },
-                        "duration": {
-                            "text": "15 mins",
-                            "value": 913
-                        },
-                        "status": "OK"
-                    }
-                ]
-            },
-            {
-                "elements": [
-                    {
-                        "distance": {
-                            "text": "5.7 km",
-                            "value": 5725
-                        },
-                        "duration": {
-                            "text": "14 mins",
-                            "value": 813
-                        },
-                        "status": "OK"
-                    },
-                    {
-                        "distance": {
-                            "text": "7.4 km",
-                            "value": 7401
-                        },
-                        "duration": {
-                            "text": "14 mins",
-                            "value": 868
-                        },
-                        "status": "OK"
-                    },
-                    {
-                        "distance": {
-                            "text": "1 m",
-                            "value": 0
-                        },
-                        "duration": {
-                            "text": "1 min",
-                            "value": 0
-                        },
-                        "status": "OK"
-                    }
-                ]
-            }
-        ],
-        "originAddresses": [
-            "4293 Perry St, Vancouver, BC V5N 3X6, Canada",
-            "2316 Nanaimo St, Vancouver, BC V5N 5E2, Canada",
-            "6154 Willingdon Ave, Burnaby, BC V5H, Canada"
-        ],
-        "destinationAddresses": [
-            "4293 Perry St, Vancouver, BC V5N 3X6, Canada",
-            "2316 Nanaimo St, Vancouver, BC V5N 5E2, Canada",
-            "6154 Willingdon Ave, Burnaby, BC V5H, Canada"
-        ]
+    let routes = [];
+    permute(dstIndexes.length, dstIndexes, (arr) => {
+        let startIndex = 0;
+        let destinationIndex = arr[0];
+        let moves = [getMoves(res, startIndex, destinationIndex)];
+        for (let i = 0; i < arr.length - 1; i++) {
+            startIndex = arr[i];
+            destinationIndex = arr[i + 1]
+            moves.push(getMoves(res, startIndex, destinationIndex));
+        }
+        routes.push({ route: moves })
     })
+    console.log(routes)
 }
 
-// function getRoutes(distanceMatrix) {
-//     let routes = [];
-//     for (let i = 0; index < distanceMatrix.rows.length; i++) {
-//         const from = distanceMatrix.originAddresses[i];
-//         for (let j = 0; j < array.length; j++) {
-//             const element = array[j];
-
-//         }
-//         const from = distanceMatrix.rows[i];
-
-//     }
-// }
+function getMoves(res, startIndex, destinationIndex) {
+    return {
+        start: res.originAddresses[startIndex],
+        stop: res.destinationAddresses[destinationIndex],
+        distance: res.rows[startIndex].elements[destinationIndex].distance,
+        time: res.rows[startIndex].elements[destinationIndex].duration
+    }
+}
 
 // GeneratingPermutation using heap algorithm
 // Reference: https://en.wikipedia.org/wiki/Heap%27s_algorithm
-function getPermutation(k, arr) {
+function permute(k, arr, callbackForSequence) {
     if (k === 1) {
-        console.log(arr)
+        if (callbackForSequence) {
+            callbackForSequence(arr)
+        }
     }
     else {
-        for (let i = 0; i < k-1; i++) {
-            getPermutation(k - 1, arr)
+        permute(k - 1, arr, callbackForSequence);
+        for (let i = 0; i < k - 1; i++) {
             if (k % 2 == 0) {
                 [arr[k - 1], arr[i]] = [arr[i], arr[k - 1]]
+                // swapArray(arr, i, k - 1);
             }
             else {
                 [arr[k - 1], arr[0]] = [arr[0], arr[k - 1]]
+                // swapArray(arr, 0, k - 1);
             }
-            getPermutation(k - 1, arr)
+            permute(k - 1, arr, callbackForSequence)
         }
     }
 }
---
+++ playground/temp2.js
@@ -0,0 +1,208 @@
+let response2 = {
+    "rows": [
+        {
+            "elements": [
+                {
+                    "distance": {
+                        "text": "1 m",
+                        "value": 0
+                    },
+                    "duration": {
+                        "text": "1 min",
+                        "value": 0
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "5.7 km",
+                        "value": 5723
+                    },
+                    "duration": {
+                        "text": "14 mins",
+                        "value": 820
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "3.5 km",
+                        "value": 3503
+                    },
+                    "duration": {
+                        "text": "8 mins",
+                        "value": 484
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "30.2 km",
+                        "value": 30150
+                    },
+                    "duration": {
+                        "text": "33 mins",
+                        "value": 1993
+                    },
+                    "status": "OK"
+                }
+            ]
+        },
+        {
+            "elements": [
+                {
+                    "distance": {
+                        "text": "5.7 km",
+                        "value": 5725
+                    },
+                    "duration": {
+                        "text": "14 mins",
+                        "value": 813
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "1 m",
+                        "value": 0
+                    },
+                    "duration": {
+                        "text": "1 min",
+                        "value": 0
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "7.4 km",
+                        "value": 7401
+                    },
+                    "duration": {
+                        "text": "14 mins",
+                        "value": 868
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "15.8 km",
+                        "value": 15775
+                    },
+                    "duration": {
+                        "text": "26 mins",
+                        "value": 1559
+                    },
+                    "status": "OK"
+                }
+            ]
+        },
+        {
+            "elements": [
+                {
+                    "distance": {
+                        "text": "3.4 km",
+                        "value": 3410
+                    },
+                    "duration": {
+                        "text": "8 mins",
+                        "value": 478
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "6.9 km",
+                        "value": 6908
+                    },
+                    "duration": {
+                        "text": "15 mins",
+                        "value": 913
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "1 m",
+                        "value": 0
+                    },
+                    "duration": {
+                        "text": "1 min",
+                        "value": 0
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "28.5 km",
+                        "value": 28501
+                    },
+                    "duration": {
+                        "text": "27 mins",
+                        "value": 1637
+                    },
+                    "status": "OK"
+                }
+            ]
+        },
+        {
+            "elements": [
+                {
+                    "distance": {
+                        "text": "30.4 km",
+                        "value": 30363
+                    },
+                    "duration": {
+                        "text": "35 mins",
+                        "value": 2123
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "16.5 km",
+                        "value": 16526
+                    },
+                    "duration": {
+                        "text": "30 mins",
+                        "value": 1807
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "29.2 km",
+                        "value": 29150
+                    },
+                    "duration": {
+                        "text": "30 mins",
+                        "value": 1814
+                    },
+                    "status": "OK"
+                },
+                {
+                    "distance": {
+                        "text": "1 m",
+                        "value": 0
+                    },
+                    "duration": {
+                        "text": "1 min",
+                        "value": 0
+                    },
+                    "status": "OK"
+                }
+            ]
+        }
+    ],
+    "originAddresses": [
+        "4293 Perry St, Vancouver, BC V5N 3X6, Canada",
+        "6154 Willingdon Ave, Burnaby, BC V5H, Canada",
+        "2316 Nanaimo St, Vancouver, BC V5N 5E2, Canada",
+        "10392 King George Blvd, Surrey, BC V3T 2W5, Canada"
+    ],
+    "destinationAddresses": [
+        "4293 Perry St, Vancouver, BC V5N 3X6, Canada",
+        "6154 Willingdon Ave, Burnaby, BC V5H, Canada",
+        "2316 Nanaimo St, Vancouver, BC V5N 5E2, Canada",
+        "10392 King George Blvd, Surrey, BC V3T 2W5, Canada"
+    ]
+}
