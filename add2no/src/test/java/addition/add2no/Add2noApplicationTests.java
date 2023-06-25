23087254f12e7b3b5836c276a1b74f7a4c8f78fb
Sanad <136133229+sanad19@users.noreply.github.com>
2023-06-25 07:17:59 +0300

    Add files via upload

---
+++ add2no/src/test/java/addition/add2no/Add2noApplicationTests.java
@@ -0,0 +1,13 @@
+package addition.add2no;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Add2noApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
