ceb35f8132f42f640f95aa9867316a7a45741ed2
Nithish0912 <136186558+Nithish0912@users.noreply.github.com>
2023-07-15 00:01:57 +0530

    Checking DirectionService Response

--- WebApp/scripts/routes.js
+++ WebApp/scripts/routes.js
@@ -12,7 +12,28 @@ function generateRoutes() {
             destinations.forEach(d => {
                 places.push(d.mapInfo.geometry.location)
             })
+            console.log(places);
+            if (destinations.length == 1){
+                const request = {
+                    origin: places[0],
+                    destination: places[1],
+                    travelMode: google.maps.TravelMode.DRIVING,
+                    unitSystem: google.maps.UnitSystem.METRIC,
+                    provideRouteAlternatives: true
+                };
+                const service = new google.maps.DirectionsService();
+                service.route(request).then((res) => {
+                    if (res) {
+                        console.log(res);
+                        getRoutesFromDirectionService(res);
+                    }
+                    else {
+                        alert("Could not found any route");
+                    }
+                })
 
+            }
+            else{
             const request = {
                 origins: places,
                 destinations: places,
@@ -22,6 +43,7 @@ function generateRoutes() {
             const service = new google.maps.DistanceMatrixService();
             service.getDistanceMatrix(request).then((res) => {
                 if (res) {
+                    console.log(res);
                     getRoutesFromDistancMatrix(res);
                 }
                 else {
@@ -29,6 +51,7 @@ function generateRoutes() {
                 }
             })
         }
+    }
         else {
             alert("Please enter valid destination");
         }
@@ -36,6 +59,35 @@ function generateRoutes() {
     else {
         alert("Please enter starting point");
     }
+}
+
+function getRoutesFromDirectionService(res) {
+    console.log(res.routes);
+    let routes = [];
+    for (let i=0; i< res.routes.length; i++){
+        let totalDistance = res.routes[i].legs[0].distance;
+        let totalTime = res.routes[i].legs[0].duration;
+        let st = res.routes[i].legs[0].start_address;
+        let ed = res.routes[i].legs[0].end_address;
+        let via = res.routes[i].summary;
+
+        let route = {
+            start: st, 
+            stop: ed, 
+            time: totalTime, 
+            distance: totalDistance, 
+            via: via
+        }
+
+        routes.push({
+            route,
+            totalDistance,
+            totalTime,
+            id: i
+        })
+    }
+
+    appData.routes = routes.sort(compareRoutes);
 }
 
 function getRoutesFromDistancMatrix(res) {
@@ -59,8 +111,8 @@ function getRoutesFromDistancMatrix(res) {
             route.push(move);
             totalDistance += move.distance.value;
             totalTime += move.time.value;
-
         }
+        console.log(route);
         routes.push({
             route,
             totalDistance,
